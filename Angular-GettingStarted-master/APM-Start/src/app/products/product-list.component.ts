aaf43864565f6261b0aa52828a8c8dbfec08162a
Alex Fuller <fulleralex47@gmail.com>
2021-02-16 07:25:16 -0500

    added product string interpolation

--- Angular-GettingStarted-master/APM-Start/src/app/products/product-list.component.ts
+++ Angular-GettingStarted-master/APM-Start/src/app/products/product-list.component.ts
@@ -4,5 +4,7 @@ import { Component } from '@angular/core';
     selector: 'pm-products',
     templateUrl: './product-list.component.html'
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+    pageTitle: string = 'Product List!';
+}
 
