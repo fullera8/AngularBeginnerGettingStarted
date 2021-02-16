bf12b7b00c13fbd43aa86f7e06edeafd47b65575
Alex Fuller <fulleralex47@gmail.com>
2021-02-16 14:20:34 -0500

    added product interface

--- Angular-GettingStarted-master/APM-Start/src/app/products/product-list.component.ts
+++ Angular-GettingStarted-master/APM-Start/src/app/products/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
@@ -6,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
-    imageWidth: number = 50; 
+    imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
-    products: any[] = [  
+    products: IProduct[] = [
         {
             "productId": 2,
             "productName": "Garden Cart",
