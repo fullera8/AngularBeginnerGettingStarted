ef282e35512556a6870a9ec05471194908960c2a
Alex Fuller <fulleralex47@gmail.com>
2021-02-18 07:47:28 -0500

    Added routing architecture

--- Angular-GettingStarted-master/APM-Start/src/app/app.component.ts
+++ Angular-GettingStarted-master/APM-Start/src/app/app.component.ts
@@ -3,9 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-    <div><h1>{{pageTitle}}</h1>
-      <pm-products></pm-products>
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand">{{pageTitle}}</a>
+      <div class="nav nav-pills">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" [routerLink] ="['/welcome']">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" [routerLink] ="['/products']">Product List</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-outlet></router-outlet>
   `
 })
 export class AppComponent {
