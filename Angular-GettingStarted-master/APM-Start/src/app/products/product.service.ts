d2c6509c65778a4d0221c524a7c823f5516dfbd2
Alex Fuller <fulleralex47@gmail.com>
2021-02-21 12:53:15 -0500

    Update product-detail page to be dynamic

--- Angular-GettingStarted-master/APM-Start/src/app/products/product.service.ts
+++ Angular-GettingStarted-master/APM-Start/src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap} from 'rxjs/operators';
+import { catchError, tap, map} from 'rxjs/operators';
 
 import { IProduct } from './product';
 
@@ -17,6 +17,13 @@ export class ProductService {
     return this.http.get<IProduct[]>(this.productURL).pipe(
       tap(data => console.log(`All: ${JSON.stringify(data)}`)),
       catchError(this.handleError)
+    );
+  }
+
+  getProductID(id: number): Observable<IProduct | undefined> {
+    return this.getProducts()
+    .pipe(
+      map((products: IProduct[]) => products.find(product => product.productId === id))
     );
   }
 
